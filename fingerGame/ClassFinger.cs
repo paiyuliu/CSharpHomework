618c32b78bf88ef15b54820cefd227abebd3cdb3
paiyuliu <paiyuliu@gmail.com>
2015-06-24 16:00:18 +0800

    初版

---
+++ fingerGame/ClassFinger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fingerGame
+{
+    internal class ClassFinger
+    {
+        private int finger;
+
+        public void Setfinger(int Finger)
+        {
+            finger = Finger;
+        }
+
+        public String Getfinger()
+        {
+            String fingerDesc;
+            if (finger == 1)
+                fingerDesc = "剪刀";
+            else if (finger == 2)
+                fingerDesc = "石頭";
+            else if (finger == 3)
+                fingerDesc = "布";
+            else
+                fingerDesc = "錯誤";
+            return fingerDesc;
+        }
+    }
+
+    internal class GameResult
+    {
+        private int peopleAns;
+        private int cupAns;
+
+        public void SetResult(int PeopleAns, int CupAns)
+        {
+            peopleAns = PeopleAns;
+            cupAns = CupAns;
+        }
+
+        private Boolean GetResult()
+        {
+            return true;
+        }
+    }
+}
---
+++ fingerGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fingerGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ClassFinger person = new ClassFinger();
+            ClassFinger computer = new ClassFinger();
+
+            Console.WriteLine("請出拳 1剪刀 2石頭 3布");
+
+            person.Setfinger(int.Parse(Console.ReadLine()));
+            Random computerFinger = new Random();
+            computer.Setfinger(computerFinger.Next(1, 4));
+            //int test = abc;
+            Console.WriteLine(person.Getfinger());
+            Console.WriteLine(computer.Getfinger());
+        }
+    }
+}
